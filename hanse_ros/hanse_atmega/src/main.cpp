141433ac64f7c720f9760bb76cdf7ea1ac0477fc
Cedric Isokeit <isokeit@informatik.uni-luebeck.de>
2011-11-17 09:56:14 +0100

    [Hanse_atmega] Main file: topics für Motoren, i2c scanner (ohne publisher)

--- hanse_ros/hanse_atmega/src/main.cpp
+++ hanse_ros/hanse_atmega/src/main.cpp
@@ -8,7 +8,8 @@
 #include <Wire.h>
 #include <std_msgs/String.h>
 #include <std_msgs/Int8.h>
-
+#include "hanse_msgs/sollSpeed.h"
+#include "utility/twi.h"
 
 //M2 Motor hinten, M1 Motor links
 #define ADDRL 0xBE>>1
@@ -20,14 +21,14 @@ ros::NodeHandle  nh;
 std_msgs::String str_msg;
 ros::Publisher chatter("chatter", &str_msg);
 
-void cbmotorleft( const std_msgs::Int8& msg){
+void cbmotorleft( const hanse_msgs::sollSpeed & msg){
   Wire.beginTransmission(ADDRL);
   Wire.send(1);
   Wire.send(msg.data);
   Wire.endTransmission();
 }  
 
-void cbmotorright( const std_msgs::Int8& msg){
+void cbmotorright( const hanse_msgs::sollSpeed& msg){
   Wire.beginTransmission(ADDRR);
   Wire.send(1);
   Wire.send(msg.data);
@@ -35,7 +36,7 @@ void cbmotorright( const std_msgs::Int8& msg){
 }
 
 
-void cbmotorback( const std_msgs::Int8& msg){
+void cbmotorback( const hanse_msgs::sollSpeed& msg){
   Wire.beginTransmission(ADDRL);
   Wire.send(2);
   Wire.send(msg.data);
@@ -43,20 +44,19 @@ void cbmotorback( const std_msgs::Int8& msg){
 }
 
 
-void cbmotorfront( const std_msgs::Int8& msg){
+void cbmotorfront( const hanse_msgs::sollSpeed& msg){
   Wire.beginTransmission(ADDRR);
   Wire.send(2);
   Wire.send(msg.data);
   Wire.endTransmission();
 }
 
-ros::Subscriber <std_msgs::Int8> motleft("/auv_name/motors/left", &cbmotorleft );
-ros::Subscriber <std_msgs::Int8> motright("/auv_name/motors/right", &cbmotorright );
-ros::Subscriber <std_msgs::Int8> motfront("/auv_name/motors/front", &cbmotorfront );
-ros::Subscriber <std_msgs::Int8> motback("/auv_name/motors/back", &cbmotorback );
+ros::Subscriber <hanse_msgs::sollSpeed> motleft("/auv_name/motors/left", &cbmotorleft );
+ros::Subscriber <hanse_msgs::sollSpeed> motright("/auv_name/motors/right", &cbmotorright );
+ros::Subscriber <hanse_msgs::sollSpeed> motfront("/auv_name/motors/front", &cbmotorfront );
+ros::Subscriber <hanse_msgs::sollSpeed> motback("/auv_name/motors/back", &cbmotorback );
 
 char hello[13] = "hello world!";
-
 
 
 void setup()
@@ -106,3 +106,18 @@ void loop()
 
   
 }
+
+void i2c_scan()
+{
+  byte cb = 0;
+  byte data = 0;
+  for( int addr = 0; addr<128;addr++){
+    cb = twi_writeTo(addr, &data, 0, 1);
+    if(cb == 0)
+     //publish i2c addr
+     //7bit shift ->
+	;
+  }
+}
+
+
