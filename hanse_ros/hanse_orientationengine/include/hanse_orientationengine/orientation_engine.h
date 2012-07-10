2ef8c9901598cc6534144a12f09415180bed348b
hajo <hajo@ubuntu.ubuntu-domain>
2012-07-10 13:26:46 +0200

    refactoring and bugfixing of gamepad, depth and orientation engine

--- hanse_ros/hanse_orientationengine/include/hanse_orientationengine/orientation_engine.h
+++ hanse_ros/hanse_orientationengine/include/hanse_orientationengine/orientation_engine.h
@@ -1,6 +1,10 @@
+#ifndef ORIENTATION_ENGINE_H
+#define ORIENTATION_ENGINE_H
+
 #include <ros/ros.h>
 #include <std_msgs/Float64.h>
 #include <std_msgs/Bool.h>
+#include <std_msgs/String.h>
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Imu.h>
 #include <dynamic_reconfigure/server.h>
@@ -14,7 +18,7 @@
 #include "hanse_srvs/Empty.h"
 #include "hanse_srvs/EngineCommand.h"
 
-#define NUM_SERVICE_LOOPS 8
+#define NUM_SERVICE_LOOPS 4
 
 class OrientationEngine {
 public:
@@ -22,61 +26,62 @@ public:
 
 private:
     // Node Handle.
-    ros::NodeHandle node;
+    ros::NodeHandle nh_;
 
     // Service for complete EngineCommand message
-    ros::ServiceServer srvHandleEngineCommand;
+    ros::ServiceServer srv_handle_engine_command_;
 
     // Services for enabling and disabling PIDs.
-    ros::ServiceServer srvEnableOrientationPid;
+    ros::ServiceServer srv_enable_orientation_pid_;
 
     // Service for enabling and disabling motors completly.
-    ros::ServiceServer srvEnableMotors;
+    ros::ServiceServer srv_enable_motors_;
 
     // Service for setting emergency stop
-    ros::ServiceServer srvSetEmergencyStop;
+    ros::ServiceServer srv_set_emergency_stop_;
 
     // Service Clients.
-    ros::ServiceClient srvClOrientationPid;
+    ros::ServiceClient srv_client_orientation_pid_;
 
     // Subscriber.
-    ros::Subscriber subVelocity;
-    ros::Subscriber subXsens;
+    ros::Subscriber sub_velocity_;
+    ros::Subscriber sub_xsens_;
+    ros::Subscriber sub_mux_selected_;
 
-    ros::Subscriber subOrientationOutput;
+    ros::Subscriber sub_orientation_output_;
 
     // Publisher.
-    ros::Publisher pubOrientationCurrent;
-    //ros::Publisher pubOrientationTarget;
+    ros::Publisher pub_orientation_current_;
 
-    ros::Publisher pubMotorLeft;
-    ros::Publisher pubMotorRight;
+    ros::Publisher pub_motor_left_;
+    ros::Publisher pub_motor_right_;
 
     // Daten Zwischenspeicher.
-    double linearSpeed;
-    double angularSpeed;
+    double linear_speed_;
+    double angular_speed_;
 
-    double orientationCurrent;
-    double orientationTarget;
-    bool orientationInit;
+    double orientation_current_;
+    double orientation_target_;
+    bool orientation_init_;
 
-    bool emergencyStop;
+    bool emergency_stop_;
 
-    bool pidsEnabled;
-    bool orientationPidEnabled;
+    bool pids_enabled_;
+    bool orientation_pid_enabled_;
 
-    bool motorsEnabled;
+    bool motors_enabled_;
+
+    bool gamepad_running_;
 
     // Standard-Nachrichten.
-    hanse_srvs::Bool enableMsg;
-    hanse_srvs::Bool disableMsg;
+    hanse_srvs::Bool enable_msg_;
+    hanse_srvs::Bool disable_msg_;
 
     // Ausgabewerte der PID-Regler.
-    int8_t orientationOutput;
+    int8_t orientation_output_;
 
-    double publishFrequency;
-
-    ros::Timer publishTimer;
+    ros::Timer publish_timer_;
+    ros::Timer gamepad_timer_;
 
     // Service-Methoden
     bool handleEngineCommand(hanse_srvs::EngineCommand::Request &req,
@@ -92,18 +97,23 @@ private:
     void velocityCallback(const geometry_msgs::Twist::ConstPtr& twist);
     void xsensCallback(const sensor_msgs::Imu::ConstPtr& xsensData);
 
-    void orientationOutputCallback(const std_msgs::Float64::ConstPtr& orientationOutput);
+    void orientationOutputCallback(const std_msgs::Float64::ConstPtr& orientation_output_);
 
     void publishTimerCallback(const ros::TimerEvent &e);
 
+    void muxSelectedCallback(const std_msgs::String::ConstPtr& topic);
+    void gamepadTimerCallback(const ros::TimerEvent &e);
+
     bool callOrientationPidEnableService(const bool msg);
 
-    void dynReconfigureCallback(hanse_orientationengine::OrientationengineConfig &config, uint32_t level);
-    hanse_orientationengine::OrientationengineConfig config;
+    void dynReconfigureCallback(hanse_orientationengine::OrientationengineConfig &config_, uint32_t level);
+    hanse_orientationengine::OrientationengineConfig config_;
 
     /** \brief dynamic_reconfigure interface */
-    dynamic_reconfigure::Server<hanse_orientationengine::OrientationengineConfig> dynReconfigureSrv;
+    dynamic_reconfigure::Server<hanse_orientationengine::OrientationengineConfig> dyn_reconfigure_srv_;
 
     /** \brief dynamic_reconfigure call back */
-    dynamic_reconfigure::Server<hanse_orientationengine::OrientationengineConfig>::CallbackType dynReconfigureCb;
+    dynamic_reconfigure::Server<hanse_orientationengine::OrientationengineConfig>::CallbackType dyn_reconfigure_cb_;
 };
+
+#endif // ORIENTATION_ENGINE_H
