8fc41fd1b76ae9f79278b0c0142afc54594717d8
Hanse <hanse@hanse-robLab.(none)>
2012-11-29 17:59:29 +0100

    Create basic structure for wall following node. Use header files. load PID values for orientation engine from launch file. connect to all usefull things in graph.

--- hanse_ros/hanse_wall_sonar/src/wall_follow.cpp
+++ hanse_ros/hanse_wall_sonar/src/wall_follow.cpp
@@ -1,36 +1,21 @@
-#include "ros/ros.h"
-#include "std_msgs/String.h"
-#include "sensor_msgs/LaserScan.h"
-#include "geometry_msgs/Twist.h"
-#include "geometry_msgs/PoseStamped.h"
-
 #include "wall_follow.h"
 
-class WallFollowNode {
-public:
-	WallFollowNode(ros::NodeHandle n) : _n(n)
-	{
-		_n.advertise<geometry_msgs::Twist>("/hanse/commands/cmd_vel", 1000);
-	}
-	
-	void sonar_laser_update(const sensor_msgs::LaserScan::ConstPtr& msg){
-    		ROS_INFO("I heard: [%d]", msg->ranges.size());
-	}
+//! \todo{TODO allow reconfiguration to use without mars}
+//! \todo{TODO subscriber in klasse auslagern?}
 
-    void pos_update(const geometry_msgs::PoseStamped::ConstPtr& msg){
-            //ROS_INFO("I heard: [%d]", msg->ranges.size());
-    }
-	
-private:
-	ros::NodeHandle _n;
-	ros::Publisher pub;
-};
 
-void sonar_laser_update(const sensor_msgs::LaserScan::ConstPtr& msg)
-{
-    //ROS_INFO("I heard: [%s]", msg->data.c_str());
-    //ROS_INFO(":D");
-    ROS_INFO("I heard: [%d]", msg->ranges.size());
+WallFollowNode::WallFollowNode(ros::NodeHandle n) : _n(n){
+   //advertise to navigation goal
+    pub = _n.advertise<geometry_msgs::PoseStamped>("/goal", 1000);
+}
+
+
+void WallFollowNode::sonar_laser_update(const sensor_msgs::LaserScan::ConstPtr& msg){
+
+}
+
+void WallFollowNode::pos_update(const geometry_msgs::PoseStamped::ConstPtr& msg){
+
 }
 
 int main(int argc, char **argv)
@@ -44,16 +29,12 @@ int main(int argc, char **argv)
     WallFollowNode follow(n);
 
     //Subscribe to topic laser_scan (from sonar)
-    ros::Subscriber sub_laser = n.subscribe("/hanse/sonar/laser_scan", 1000, boost::bind(&WallFollowNode::sonar_laser_update, &follow, _1));
+    ros::Subscriber sub_laser = n.subscribe<sensor_msgs::LaserScan>("/hanse/sonar/laser_scan", 1000, boost::bind(&WallFollowNode::sonar_laser_update, &follow, _1));
 
-    ros::Subscriber sub_pos = n.subscribe("/hanse/posemeter", 1000, boost::bind(&WallFollowNode::sonar_laser_update, &follow, _1));
-
-    //TODO Publisher an Navigation anpassen!
-    ros::Publisher pub = n.advertise<geometry_msgs::Twist>("/hanse/commands/cmd_vel", 1000);
+    //Subscribe to the current position
+    ros::Subscriber sub_pos = n.subscribe<geometry_msgs::PoseStamped>("/hanse/posemeter", 1000, boost::bind(&WallFollowNode::pos_update, &follow, _1));
     
-
     ros::Rate loop_rate(10);
-
 
     ros::spin();
 
